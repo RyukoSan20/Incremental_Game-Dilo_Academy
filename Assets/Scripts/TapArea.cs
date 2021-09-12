6966221d0968074556a0784677e1962ab682b6bd
RyukoSans <78121811+RyukoSan20@users.noreply.github.com>
2021-09-13 01:24:44 +0700

    Add files via upload

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+    public AudioSource gameClip;
+
+    public void OnPointerDown (PointerEventData eventData)
+
+    {
+        // Script by Hariz Abdurrafi
+        // https://github.com/RyukoSan20/Incremental_Game-Dilo_Academy
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+        gameClip.Play();
+
+    }
+
+}
